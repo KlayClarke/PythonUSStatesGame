30c622e5a624fb3a0d6d302c75ede5616b25db3c
Klay Clarke <klayaclarke@gmail.com>
2021-11-19 13:58:51 -0500

    screen keeps track of number of correct guesses - if user guesses all states, screen clears and user sees a message stating that they've guessed all states

--- main.py
+++ main.py
@@ -12,18 +12,36 @@ state_name = turtle.Turtle()
 state_name.penup()
 state_name.hideturtle()
 
-answer_state = screen.textinput(title='Guess The State', prompt='What\'s another state\'s name?').title()
-
 data = pandas.read_csv('50_states.csv')
-for state in data['state']:
-    if answer_state == state:
-        state_data = data[data.state == f'{state}']
-        state_x_coordinate = int(state_data.x)
-        state_y_coordinate = int(state_data.y)
-        state_coordinates = (state_x_coordinate, state_y_coordinate)
-        # pandas.
-        state_name.goto(state_coordinates)
-        state_name.write(arg=state, move=False, align='center', font=('Arial', 8, 'normal'))
 
+correct_answers = []
+score = 0
+
+game_is_on = True
+
+while game_is_on:
+
+    answer_state = screen.textinput(title=f'{len(correct_answers)}/50 correct',
+                                    prompt='What\'s another state\'s name?').title()
+
+    for state in data['state']:
+        score = len(correct_answers)
+        if answer_state == state:
+            if state in correct_answers:
+                pass
+            else:
+                correct_answers.append(state)
+                state_data = data[data.state == f'{state}']
+                state_x_coordinate = int(state_data.x)
+                state_y_coordinate = int(state_data.y)
+                state_coordinates = (state_x_coordinate, state_y_coordinate)
+                # pandas.
+                state_name.goto(state_coordinates)
+                state_name.write(arg=state, move=False, align='center', font=('Arial', 8, 'normal'))
+        elif score == 50:
+            screen.clear()
+            state_name.write(arg='You Guessed All States', move=False, align='center', font=('Arial', 50, 'normal'))
+            state_name.goto(0, 0)
+            game_is_on = False
 
 turtle.mainloop()
