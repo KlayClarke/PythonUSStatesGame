acabf46c0b7305cf5609c7cdc3fa3f1c91dd9b54
Klay Clarke <klayaclarke@gmail.com>
2021-11-19 09:25:54 -0500

    when a state is clicked, console will print coordinates

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import turtle
 
-
 screen = turtle.Screen()
 screen.title('U.S. States Game')
 image = 'blank_states_img.gif'
@@ -9,4 +8,10 @@ screen.addshape(image)
 turtle.shape(image)
 
 
-screen.exitonclick()
+def get_click_coordinates(x, y):
+    print(x, y)
+
+
+turtle.onscreenclick(get_click_coordinates)
+
+turtle.mainloop()
