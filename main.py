8d605bf1462e0c379a48fd91ca74f6fb4dd0e842
Klay Clarke <klayaclarke@gmail.com>
2021-11-20 12:30:25 -0500

    if user inputs 'exit', break out of the game loop

--- main.py
+++ main.py
@@ -23,7 +23,8 @@ while game_is_on:
 
     answer_state = screen.textinput(title=f'{len(correct_answers)}/50 correct',
                                     prompt='What\'s another state\'s name?').title().strip()
-
+    if answer_state == 'Exit':
+        break
     for state in data['state']:
         score = len(correct_answers)
         if answer_state == state:
@@ -35,7 +36,6 @@ while game_is_on:
                 state_x_coordinate = int(state_data.x)
                 state_y_coordinate = int(state_data.y)
                 state_coordinates = (state_x_coordinate, state_y_coordinate)
-                # pandas.
                 state_name.goto(state_coordinates)
                 state_name.write(arg=state, move=False, align='center', font=('Arial', 8, 'normal'))
         elif score == 50:
