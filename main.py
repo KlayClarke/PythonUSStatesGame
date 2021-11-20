19f3c4719e1e3c01d50b1bd1c6d9ba53dd6de17b
Klay Clarke <klayaclarke@gmail.com>
2021-11-20 13:26:07 -0500

    fixed bug that placed commas between each character when writing a csv file of the missed answers

--- main.py
+++ main.py
@@ -28,30 +28,30 @@ while game_is_on:
     if answer_state == 'Exit':
         for state in data['state']:
             if state not in correct_answers:
-                missed_answers.append(state)
-
+                missed_answers.append([state])
         with open('missed_answers.csv', mode='w') as file:
             writer = csv.writer(file)
-            writer.writerow(missed_answers)
+            writer.writerows(missed_answers)
         print(missed_answers)
         break
+    else:
+        for state in data['state']:
+            score = len(correct_answers)
+            if answer_state == state:
+                if state in correct_answers:
+                    pass
+                else:
+                    correct_answers.append(state)
+                    state_data = data[data.state == f'{state}']
+                    state_x_coordinate = int(state_data.x)
+                    state_y_coordinate = int(state_data.y)
+                    state_coordinates = (state_x_coordinate, state_y_coordinate)
+                    state_name.goto(state_coordinates)
+                    state_name.write(arg=state, move=False, align='center', font=('Arial', 8, 'normal'))
+            elif score == 50:
+                screen.clear()
+                state_name.write(arg='You Guessed All States', move=False, align='center', font=('Arial', 50, 'normal'))
+                state_name.goto(0, 0)
+                game_is_on = False
 
-    for state in data['state']:
-        score = len(correct_answers)
-        if answer_state == state:
-            if state in correct_answers:
-                pass
-            else:
-                correct_answers.append(state)
-                state_data = data[data.state == f'{state}']
-                state_x_coordinate = int(state_data.x)
-                state_y_coordinate = int(state_data.y)
-                state_coordinates = (state_x_coordinate, state_y_coordinate)
-                state_name.goto(state_coordinates)
-                state_name.write(arg=state, move=False, align='center', font=('Arial', 8, 'normal'))
-        elif score == 50:
-            screen.clear()
-            state_name.write(arg='You Guessed All States', move=False, align='center', font=('Arial', 50, 'normal'))
-            state_name.goto(0, 0)
-            game_is_on = False
     turtle.mainloop()
